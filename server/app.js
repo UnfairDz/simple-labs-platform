00534f0e4cde87d35f54451cb08474d4d720448d
Brikci Sid Ramzi <brikci.sid.ramzi@gmail.com>
2023-06-23 21:25:33 +0100

    Allowing acces to the api and adding some data security

--- server/app.js
+++ server/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 
-//const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const cors = require("cors");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -13,7 +15,7 @@ const app = express();
 
 // 1) GLOBAL MIDDLEWARES
 // Implement CORS
-//app.use(cors());
+app.use(cors());
 // app.use(
 //   cors({
 //     credentials: true,
@@ -21,17 +23,17 @@ const app = express();
 //   })
 // );
 
-// app.options("*", cors());
+//app.options("*", cors());
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true, limit: "10kb" }));
 
 // Data sanitization against NoSQL query injection
-//app.use(mongoSanitize());
+app.use(mongoSanitize());
 
 // Data sanitization against XSS
-//app.use(xss());
+app.use(xss());
 
 // 3) ROUTES
 app.use("/api/v1/labs", labRouter);
