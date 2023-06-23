08266efd6d87d299a6f59018f5551e9f7ed48842
Brikci Sid Ramzi <brikci.sid.ramzi@gmail.com>
2023-06-23 22:01:54 +0100

    Connecting the backend to the add new lab form page

--- client/src/app/labs/new/page.tsx
+++ client/src/app/labs/new/page.tsx
@@ -1,50 +1,77 @@
 "use client";
 
 import React, { useState } from 'react'
+import { useRouter } from 'next/navigation';
 import './add-lab.css'
 
-import { judson, jua } from '../../fonts'
+import { jua } from '../../fonts'
+
+import { useDispatch } from 'react-redux';
+import {
+  createLab,
+} from '../../../redux/features/labSlice';
+import axios from 'axios';
 
 
 function AddLabForm() {
   const [lab, setLab] = useState({})
 
+  const dispatch = useDispatch();
+  const { push } = useRouter();
+
   const handleChange = (e: any) => {
     setLab({...lab, [e.target.name]: e.target.value});
   }
 
-  const handleSubmit = (e:any) => {
+  const handleSubmit = async (e:any) => {
     e.preventDefault();
-  }
+    
+    // Make POST request to server with task data
+    await axios.post('http://localhost:5000/api/v1/labs', lab, {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(response => {
+      console.log(`Lab created successfully`);
+      push('/');
+    })
+    .catch(error => {
+      console.error(`Failed to create lab: ${error.message}`);
+      // Handle the error, such as displaying an error message
+    })
+
+    dispatch(createLab(lab));
+  };
 
   return (
     <div className='content-container'>
-      <form className='add-lab-form'>
+      <form onSubmit={handleSubmit} className='add-lab-form'>
           <label id='jua-font' className={jua.className + ' form-label'}>Name</label>
           <br/>
-          <input id='jua-font' className={jua.className + ' form-input'} type="text" name="name" onChange={handleChange} />
+          <input id='jua-font' className={jua.className + ' form-input'} type="text" name="name" onChange={handleChange} required />
           <br/>
           <label id='jua-font' className={jua.className + ' form-label'}>Technologies</label>
           <br/>
-          <input id='jua-font' className={jua.className + ' form-input'} type="text" name="technology" onChange={handleChange} />
+          <input id='jua-font' className={jua.className + ' form-input'} type="text" name="technology" onChange={handleChange} required />
           <br/>
           <label id='jua-font' className={jua.className + ' form-label'}>Description</label>
           <br/>
-          <textarea id='jua-font' className={jua.className + ' form-input text-area'} name="description" onChange={handleChange} />
+          <textarea id='jua-font' className={jua.className + ' form-input text-area'} name="description" onChange={handleChange} required />
           <br/>
           <label id='jua-font' className={jua.className + ' form-label'}>Capacity</label>
           <br/>
-          <input id='jua-font' className={jua.className + ' form-input'} type="number" name="capacity" onChange={handleChange} />
+          <input id='jua-font' className={jua.className + ' form-input'} type="number" name="capacity" onChange={handleChange} required />
           <br/>
           <label id='jua-font' className={jua.className + ' form-label'}>Start date</label>
           <br/>
-          <input id='jua-font' className={jua.className + ' form-input'} type="date" name="startDate" onChange={handleChange} />
+          <input id='jua-font' className={jua.className + ' form-input'} type="date" name="startDate" onChange={handleChange} required />
           <br/>
           <label id='jua-font' className={jua.className + ' form-label'}>End date</label>
           <br/>
-          <input id='jua-font' className={jua.className + ' form-input'} type="date" name="endDate" onChange={handleChange} />
+          <input id='jua-font' className={jua.className + ' form-input'} type="date" name="endDate" onChange={handleChange} required />
           <br/>
-          <button id='jua-font' className='submit-btn' onClick={handleSubmit}>Create</button>
+          <button id='jua-font' className='submit-btn'>Create</button>
       </form>
     </div>
   )
